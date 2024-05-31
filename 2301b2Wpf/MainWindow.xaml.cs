61b386b60d8de03dc209e0acfaecb5b99823247a
harisnaseer24 <harisnaseer258@gmail.com>
2024-05-31 11:08:49 +0500

    first WPF Application

---
+++ 2301b2Wpf/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace _2301b2Wpf
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+        
+        string name = textBox1.Text;
+            string age  = textBox2.Text;
+
+            MessageBox.Show($"Hello, {name}");
+            textBox1.Clear();
+            textBox2.Clear();
+
+            nameblock.Text =  "Name : "+name;
+            ageblock.Text = "Age: "+age + "years old.";
+        }
+    }
+}
